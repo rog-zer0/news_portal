effe62f2f0c1ef36ea3ac72a351adc9c3d0d7c02
zer0 <129584083+rog-zer0@users.noreply.github.com>
2023-12-06 16:19:31 +0300

    Выводите новости в следующем виде — заголовок, дата публикации в формате день.месяц.год, затем первые 20 слов текста статьи. Можно вывести как списком, так и таблицей. Новости должны выводиться в порядке от более свежей к старой. Сверху страницы должно быть выведено количество всех новостей (используется фильтр news|length). По ссылке /news/<id новости> должна выводиться детальная информация о новости. Заголовок, дата публикации в формате день.месяц.год и полный текст статьи.

--- news/views.py
+++ news/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import News
 
@@ -13,6 +14,13 @@ class NewsList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['news'] = News.objects.all()
+        return context
+
 
 # Создаем свой класс, который наследуется от ListView.
 # Указываем модель, из которой будем выводить данные.
