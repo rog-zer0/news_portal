b069e7b60730c50c48f2fee7f3862c653583d9b4
zer0 <129584083+rog-zer0@users.noreply.github.com>
2024-01-20 13:36:30 +0300

    Реализована пагинация

--- news/views.py
+++ news/views.py
@@ -1,34 +1,33 @@
 from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import News
-
+from .filters import NewsFilter
+from django.db.models import Q
+from django.shortcuts import render
 
 class NewsList(ListView):
-    # Указываем модель, объекты которой мы будем выводить
     model = News
-    # Поле, которое будет использоваться для сортировки объектов
     ordering = 'updated_at'
-    # Указываем имя шаблона, в котором будут все инструкции о том,
-    # как именно пользователю должны быть показаны наши объекты
     template_name = 'news.html'
-    # Это имя списка, в котором будут лежать все объекты.
-    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'news'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = NewsFilter(self.request.GET, queryset)
+        return self.filterset.qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time_now'] = datetime.utcnow()
-        context['news'] = News.objects.all()
+        # context['news'] = News.objects.all()
+        context['filterset'] = self.filterset
         return context
 
-
-# Создаем свой класс, который наследуется от ListView.
-# Указываем модель, из которой будем выводить данные.
-# Указываем поле сортировки данных модели (необязательно).
-# Записываем название шаблона.
-# Объявляем, как хотим назвать переменную в шаблоне.
 
 class NewsDetail(DetailView):
     model = News
     template_name = 'any_news.html'
     context_object_name = 'news'
+
+
